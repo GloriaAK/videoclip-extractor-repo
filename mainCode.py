29c2ef3a8db60c69e977c4235f634c96a42dca0f
Gloria Kolyabin <gloria.kluth@gmail.com>
2023-03-16 18:46:28 -0400

    major refactor; added mediaManipulator() and clipAssembler()

--- mainCode.py
+++ mainCode.py
@@ -2,59 +2,66 @@
 from moviepy.editor import *
 from moviepy.video.tools.subtitles import SubtitlesClip
 
-# variables for uploaded SRT file and video start and end times to be clipped
+# variables
 srtSubs = 'ProjectMedia/I_LOVE_TO_SLEEP_twoSS.srt'
+green_back_img = "ProjectMedia/green_back.png"
+green_ender_img = "ProjectMedia/green_final.png"
+video = "ProjectMedia/Clip-2-I-like-to-sleep.mp4"
 video_start = "0:00:00"
 video_end = "0:01:00"
 
+
 # takes a srt file, a start time, an end time, and extracts subtitles between the time specified
-def subtitleExtractor(sub_file, vid_st, vid_en):
+def subtitleExtractor(sub_file, vid_st, vid_end):
 	generator = lambda txt: TextClip(txt, font='Calibri', fontsize=75, color='white')
 	subs = SubtitlesClip(sub_file, generator)
 	subtitles = SubtitlesClip(subs, generator)
-	subtitle_edits1 = subtitles.subclip(vid_st, vid_en)
+	subtitle_edits1 = subtitles.subclip(vid_st, vid_end)
 	subtitle_edits2 = subtitle_edits1.set_position('top').margin(top=20, opacity=0)
 
 	return subtitle_edits2
 
+
+# extracts a clip/image using the specified start and end times, adjusts the aspect ratio, and does volume control
+def mediaManipulator(med, vid_st, vid_end, placement):
+	media = ImageClip(med).subclip(vid_st, vid_end)
+
+	# determine how to resize the image to fit 9:16 aspect ratio
+	# when y2=less, img=less: for clip2, when x1=more, left side cuts more, when x2=more, right side cuts less:
+	if placement == "T":  # if it's placed at the top
+		resized_img = media.crop(x1=0, x2=0, y1=0, y2=540)
+		return resized_img
+	elif placement == "B":  # if it's placed at the bottom
+		resized_img = media.crop(x1=0, x2=0, y1=1380, y2=0)
+		return resized_img
+	elif placement == "V":  # if it's a video
+		adjusted_vid = media.fx(afx.volumex, 3)
+		resized_vid = adjusted_vid.crop(x1=120, x2=840, y1=0, y2=0).resize(1.5)
+		return resized_vid
+	else:  # else if it's the ender image
+		return media
+
+
+# assembles the clip and renders it
+def clipAssembler(top_img, clip, bottom_img, subs, end_img):
+	# creating the overlay with resized clips
+	adding_clips = clips_array([[top_img], [clip], [bottom_img]])
+	body = adding_clips.resize( (1080, 1920) )
+	body_with_subs = CompositeVideoClip([body, subs])
+
+	# cut six or less seconds of video
+	with_cutting = body_with_subs.cutout(0, 0)
+
+	# add the ender clip, title the video, and render
+	final_clip = concatenate_videoclips([with_cutting, end_img])
+	final_clip.write_videofile("renewed_code5.mp4")
+
+
+# calling the functions
+top_image = mediaManipulator(green_back_img, video_start, video_end, "T")
+bottom_image = mediaManipulator(green_back_img, video_start, video_end, "B")
+ender_image = mediaManipulator(green_ender_img, vid_st="0:00:00", vid_end="0:00:02")
+video_clip = mediaManipulator(video, video_start, video_end, "V")
 extracted_subs = subtitleExtractor(srtSubs, video_start, video_end)
+clipAssembler(top_image, video_clip, bottom_image, extracted_subs, ender_image)
 
-# import and clip videos, as well as volume control
-clip1 = ImageClip("ProjectMedia/green_back.png").subclip(video_start, video_end)
-clip2 = VideoFileClip("ProjectMedia/Clip-2-I-like-to-sleep.mp4").subclip(video_start, video_end).fx(afx.volumex, 3)
-clip3 = ImageClip("ProjectMedia/green_back.png").subclip(video_start, video_end)
-ender_clip = ImageClip("ProjectMedia/green_final.png").subclip("0:00:00", "0:00:02")
-
-# resize clips for 9:16 ratio format
-# when y2=less, img=less: for clip2, when x1=more, left side cuts more, when x2=more, right side cuts less:
-resized_clip1 = clip1.crop(x1=0, x2=0, y1=0, y2=540)
-resized_clip2 = clip2.crop(x1=120, x2=840, y1=0, y2=0).resize(1.5)
-resized_clip3 = clip3.crop(x1=0, x2=0, y1=1380, y2=0)
-
-# creating the overlay with resized clips
-adding_clips = clips_array([[resized_clip1], [resized_clip2], [resized_clip3]])
-body = adding_clips.resize( (1080, 1920) )
-body_with_subs = CompositeVideoClip([body, extracted_subs])
-
-# cut six or less seconds of video
-with_cutting = body_with_subs.cutout(5, 6)
-
-# add the ender clip, title the video, and render
-final_clip = concatenate_videoclips([with_cutting, ender_clip])
-final_clip.write_videofile("renewed_code5.mp4")
-
-'''
-# this is a subtitle adder
-from moviepy.editor import *
-from moviepy.video.tools.subtitles import SubtitlesClip
-
-generator = lambda txt: TextClip(txt, font='Arial', fontsize=34, color='white')
-subs = SubtitlesClip('ProjectMedia/I_LOVE_TO_SLEEP_twoSS.srt', generator)
-subtitles = SubtitlesClip(subs, generator)
-
-video = VideoFileClip("ProjectMedia/Clip-2-I-like-to-sleep.mp4")
-result = CompositeVideoClip([video, subtitles.set_pos(('center', 'bottom'))])
-
-result.write_videofile("testingStuff_Vid2.mp4")
-how to printout the runtime envirnment within an exe in windows 10
-'''
