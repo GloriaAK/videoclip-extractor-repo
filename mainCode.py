c5b6be86217a3e1df736821264aaa10bf63c834c
gabe <kluth.house@gmail.com>
2023-03-17 20:32:02 -0400

    bug fixes for latest refactor

--- mainCode.py
+++ mainCode.py
@@ -8,7 +8,7 @@ green_back_img = "ProjectMedia/green_back.png"
 green_ender_img = "ProjectMedia/green_final.png"
 video = "ProjectMedia/Clip-2-I-like-to-sleep.mp4"
 video_start = "0:00:00"
-video_end = "0:01:00"
+video_end = "0:00:05"
 
 
 # takes a srt file, a start time, an end time, and extracts subtitles between the time specified
@@ -24,21 +24,24 @@ def subtitleExtractor(sub_file, vid_st, vid_end):
 
 # extracts a clip/image using the specified start and end times, adjusts the aspect ratio, and does volume control
 def mediaManipulator(med, vid_st, vid_end, placement):
-	media = ImageClip(med).subclip(vid_st, vid_end)
 
 	# determine how to resize the image to fit 9:16 aspect ratio
 	# when y2=less, img=less: for clip2, when x1=more, left side cuts more, when x2=more, right side cuts less:
 	if placement == "T":  # if it's placed at the top
+		media = ImageClip(med).subclip(vid_st, vid_end)
 		resized_img = media.crop(x1=0, x2=0, y1=0, y2=540)
 		return resized_img
 	elif placement == "B":  # if it's placed at the bottom
+		media = ImageClip(med).subclip(vid_st, vid_end)
 		resized_img = media.crop(x1=0, x2=0, y1=1380, y2=0)
 		return resized_img
 	elif placement == "V":  # if it's a video
+		media = VideoFileClip(med).subclip(vid_st, vid_end)
 		adjusted_vid = media.fx(afx.volumex, 3)
 		resized_vid = adjusted_vid.crop(x1=120, x2=840, y1=0, y2=0).resize(1.5)
 		return resized_vid
 	else:  # else if it's the ender image
+		media = ImageClip(med).subclip(vid_st, vid_end)
 		return media
 
 
@@ -54,14 +57,23 @@ def clipAssembler(top_img, clip, bottom_img, subs, end_img):
 
 	# add the ender clip, title the video, and render
 	final_clip = concatenate_videoclips([with_cutting, end_img])
-	final_clip.write_videofile("renewed_code5.mp4")
+	final_clip.write_videofile("OOCodeResult1.mp4")
 
 
 # calling the functions
 top_image = mediaManipulator(green_back_img, video_start, video_end, "T")
 bottom_image = mediaManipulator(green_back_img, video_start, video_end, "B")
-ender_image = mediaManipulator(green_ender_img, vid_st="0:00:00", vid_end="0:00:02")
+ender_image = mediaManipulator(green_ender_img, vid_st="0:00:00", vid_end="0:00:02", placement="E")
 video_clip = mediaManipulator(video, video_start, video_end, "V")
 extracted_subs = subtitleExtractor(srtSubs, video_start, video_end)
 clipAssembler(top_image, video_clip, bottom_image, extracted_subs, ender_image)
-
+# @decorator.decorator
+# def apply_to_audio(f, clip, *a, **k):
+#     """ This decorator will apply the function f to the audio of
+#         the clip created with f """
+#
+#     newclip = f(clip, *a, **k)
+#     if getattr(newclip, 'audio', None):
+#         newclip.audio = f(newclip.audio, *a, **k) #change second newclip to clip
+#     return newclip
+# 
