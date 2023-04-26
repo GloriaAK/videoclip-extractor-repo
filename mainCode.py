4ca47307aa357fdcbff5ea2820af5cb5508d7d81
gabe <kluth.house@gmail.com>
2023-04-26 14:20:55 -0400

    changes allowing for different fps

--- mainCode.py
+++ mainCode.py
@@ -3,12 +3,19 @@ from moviepy.editor import *
 from moviepy.video.tools.subtitles import SubtitlesClip
 
 # variables
-srtSubs = 'ProjectMedia/I_LOVE_TO_SLEEP_twoSS.srt'
+
+# srtSubs = 'ProjectMedia/I_LOVE_TO_SLEEP_twoSS.srt'
+# green_back_img = "ProjectMedia/green_back.png"
+# green_ender_img = "ProjectMedia/green_final.png"
+# video = "ProjectMedia/Clip-2-I-like-to-sleep.mp4"
+# video_start = "0:00:01"
+# video_end = "0:00:04"
+srtSubs = 'C:\\Users\\Gabriel\\OneDrive\\MBC Folder\\29-Gen-Z\\29a-Raw Clips\\20230416 clips\\auto_generated_srt_captions.srt'
 green_back_img = "ProjectMedia/green_back.png"
 green_ender_img = "ProjectMedia/green_final.png"
-video = "ProjectMedia/Clip-2-I-like-to-sleep.mp4"
-video_start = "0:00:00"
-video_end = "0:00:05"
+video = "C:\\Users\\Gabriel\\OneDrive\\MBC Folder\\21-Broadcast\\21f-Raw Sermon Video\\Saved Sermons\\Faithful Men In Evil Days.mp4"
+video_start = "0:27:32"
+video_end = "0:27:35"
 
 
 # takes a srt file, a start time, an end time, and extracts subtitles between the time specified
@@ -37,8 +44,14 @@ def mediaManipulator(med, vid_st, vid_end, placement):
 		return resized_img
 	elif placement == "V":  # if it's a video
 		media = VideoFileClip(med).subclip(vid_st, vid_end)
-		adjusted_vid = media.fx(afx.volumex, 3)
-		resized_vid = adjusted_vid.crop(x1=120, x2=840, y1=0, y2=0).resize(1.5)
+		adjusted_vid = media.fx(afx.volumex, 3)  # multiplying the audio levels by three
+		if round(media.fps, 3) == 20.000:  # if it's a video filmed at 20 frames per second, use these crop numbers
+			resized_vid = adjusted_vid.crop(x1=160, x2=1120, y1=0, y2=0).resize(1.125)
+		elif round(media.fps, 3) == 23.976:  # if it's a video filmed at 24 frames per second, use these crop numbers
+			resized_vid = adjusted_vid.crop(x1=120, x2=840, y1=0, y2=0).resize(1.5)
+		else:
+			resized_vid = adjusted_vid.crop(x1=120, x2=840, y1=0, y2=0).resize(1.5)  # else, you're pretty much screwed, just do whatever
+
 		return resized_vid
 	else:  # else if it's the ender image
 		media = ImageClip(med).subclip(vid_st, vid_end)
@@ -57,7 +70,7 @@ def clipAssembler(top_img, clip, bottom_img, subs, end_img):
 
 	# add the ender clip, title the video, and render
 	final_clip = concatenate_videoclips([with_cutting, end_img])
-	final_clip.write_videofile("OOCodeResult1.mp4")
+	final_clip.write_videofile("CodeResult5.mp4")
 
 
 # calling the functions
--- videoclipManipulator.py
+++ videoclipManipulator.py
@@ -1,21 +1,36 @@
 from moviepy.editor import *
-video_start = "0:00:00"
-video_end = "0:00:35"
+video_start = "0:32:20"
+video_end = "0:32:23"
 clip1 = ImageClip("ProjectMedia/green_back.png").subclip(video_start, video_end)
-clip2 = VideoFileClip("ProjectMedia/Clip-2-I-like-to-sleep.mp4").subclip(video_start, video_end).fx(afx.volumex, 3)
+new_clips = VideoFileClip("C:\\Users\\Gabriel\\OneDrive\\MBC Folder\\21-Broadcast\\21f-Raw Sermon Video\\Pay Attention To Last Words.mp4").subclip(video_start, video_end)
+old_clips = VideoFileClip("ProjectMedia\Clip-1-smash-and-grab.mp4").subclip("0:00:01", "0:00:04")
 clip3 = ImageClip("ProjectMedia/green_back.png").subclip(video_start, video_end)
 ender_clip = ImageClip("ProjectMedia/green_final.png").subclip("0:00:00", "0:00:02")
 # when y2=less, img=less: for clip2, when x1=more, left side cuts more, when x2=more, right side cuts less:
-resized_clip1 = clip1.crop(x1=0, x2=0, y1=0, y2=540)
-resized_clip2 = clip2.crop(x1=120, x2=840, y1=0, y2=0).resize(1.5)
-resized_clip3 = clip3.crop(x1=0, x2=0, y1=1380, y2=0)
+
+if round(old_clips.fps, 3) == 20.000:
+    resized_clip1 = clip1.crop(x1=0, x2=0, y1=0, y2=540)
+    resized_clip2 = new_clips.crop(x1=160, x2=1120, y1=0, y2=0).resize(1.125)
+    resized_clip3 = clip3.crop(x1=0, x2=0, y1=1380, y2=0)
+elif round(old_clips.fps, 3) == 23.976:
+    resized_clip1 = clip1.crop(x1=0, x2=0, y1=0, y2=540)
+    resized_clip2 = new_clips.crop(x1=120, x2=840, y1=0, y2=0).resize(1.5)
+    resized_clip3 = clip3.crop(x1=0, x2=0, y1=1380, y2=0)
+else:
+    print("Something is wrong")
+# print(clip2.fps)
+# print(round(clip__2.fps, 3))
+# resized_clip1 = clip1.crop(x1=0, x2=0, y1=0, y2=540)
+# resized_clip2 = clip2.crop(x1=120, x2=840, y1=0, y2=0).resize(1.5)
+# resized_clip3 = clip3.crop(x1=0, x2=0, y1=1380, y2=0)
+
+# resized_clip1 = clip1.crop(x1=0, x2=0, y1=0, y2=540)
+# resized_clip2 = clip2.crop(x1=160, x2=1120, y1=0, y2=0).resize(1.125)
+# resized_clip3 = clip3.crop(x1=0, x2=0, y1=1380, y2=0)
 
 adding_clips = clips_array([[resized_clip1], [resized_clip2], [resized_clip3]])
-body = adding_clips.resize( (1080, 1920) )
+body = adding_clips.resize((1080, 1920))
 
-# cut_start = '0:00:08:00'
-# cut_end = '0:00:24:00'
-final_body = body.cutout("0:00:08", "0:00:24")  # ‘01:03:05.35’
 
-final_clip = concatenate_videoclips([final_body, ender_clip])
-final_clip.write_videofile("vid_clip_Man7.mp4")
+final_clip = concatenate_videoclips([body, ender_clip])
+final_clip.write_videofile("resize_testing5.mp4")
